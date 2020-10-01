329a6876105e1d08416822f0da08b1d0318f0249
Emde <mortenemdec@gmail.com>
2020-10-01 11:05:00 +0200

    transfer template to app and set up mock data

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export default {
+  products: [
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: './images/d1.jpg',
+      price: 60,
+      brand: 'Mike',
+      raiting: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: './images/d2.jpg',
+      price: 55,
+      brand: 'Abidas',
+      raiting: 3.5,
+      numReviews: 19
+    },
+    {
+      name: 'Regular Shirt',
+      category: 'Shirts',
+      image: './images/d3.jpg',
+      price: 45,
+      brand: 'Bold Navy',
+      raiting: 4.0,
+      numReviews: 32
+    },
+    {
+      name: 'Slim Pants',
+      category: 'Pants',
+      image: './images/d4.jpg',
+      price: 80,
+      brand: 'Mike',
+      raiting: 4.0,
+      numReviews: 12
+    },
+    {
+      name: 'High Pants',
+      category: 'Pantss',
+      image: './images/d5.jpg',
+      price: 75,
+      brand: 'Fuma',
+      raiting: 3.0,
+      numReviews: 7
+    },
+    {
+      name: 'Regular Pants',
+      category: 'Pants',
+      image: './images/d6.jpg',
+      price: 70,
+      brand: 'Hugo Hoss',
+      raiting: 4.5,
+      numReviews: 23
+    },
+  ]
+}
